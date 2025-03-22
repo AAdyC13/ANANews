5a0143a40606fb9502b3c78ca17132628cef6514
AAdyC13 <aadycheng@gmail.com>
2025-03-22 20:03:52 +0800

    代碼測試成功，未完成爬蟲同步功能

--- core/admin.py
+++ core/admin.py
@@ -3,18 +3,19 @@ from .models import analysed_news
 from .models import system_config
 # Register your models here.
 
+
 @admin.register(analysed_news)
 class analysed_news(admin.ModelAdmin):
-    list_display = (
-    'title','category','date','news_id_one', 'news_id_two',    'content', 
-    'sentiment', 'summary', 'top_key_freq', 'tokens', 'tokens_v2', 
-    'entities', 'token_pos', 'photo_link'
-)  # 在後台顯示的欄位
-    search_fields = ('news_id_one', 'news_id_two', 'date', 'category', 'title')  # 可搜尋的欄位
-    
+    list_display = ('title', 'category', 'date', 'news_id_one',
+                    'news_id_two', 'content', 'photo_link')  # 在後台顯示的欄位
+
+    search_fields = ('news_id_one', 'news_id_two', 'date',
+                     'category', 'title')  # 可搜尋的欄位
+
+
 @admin.register(system_config)
 class system_config(admin.ModelAdmin):
     list_display = (
-    'sysdb_id','sysdb_name','sysdb_data'
-)  # 在後台顯示的欄位
+        'sysdb_id', 'sysdb_name', 'sysdb_data'
+    )  # 在後台顯示的欄位
     search_fields = ('sysdb_id', 'sysdb_name')  # 可搜尋的欄位
